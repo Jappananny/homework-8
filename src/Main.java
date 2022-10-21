3844ef02ed498ca22d0eb4050ed14039ca1712e9
Jappananny <Jappananny@gmail.com>
2022-10-21 22:18:08 +0300

    ДЗ8-4

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        //Задача 3
-        System.out.println("Задача 3");
-        int[] arrNamber1 = generateRandomArray();
-        float totalSum=0;
-        for (int i = 0; i < arrNamber1.length; i++) {
-            totalSum += arrNamber1[i];
+        //Задача 4
+        System.out.println("Задача 4");
+
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length-1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
-        totalSum = totalSum / arrNamber1.length;
-        System.out.println("Средняя сумма трат за месяц составила "+totalSum+"рублей");
 
-
-
-
-
-    }
-
-
-
-
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
-        }
-        return arr;
     }
 }
